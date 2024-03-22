fe88a14a8a95da993f414123f26091ceb404e584
Melipona <shvybzik@outlook.com>
2024-03-22 20:07:21 +0800

    图标与依赖
    1. 引入组件化的矢量图图标，写在/test页面内
    2. 添加sass作为开发依赖项
    3. 添加背景图片

---
+++ magic-meli-web/src/components/BackgroundImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <img src="/src/assets/graphs/Synth.png" />
+</template>
+
+<style lang="scss" scoped>
+@import '/src/assets/styles/mixins.scss';
+
+img {
+  @include PageCenter;
+  z-index: -1;
+}
+</style>
