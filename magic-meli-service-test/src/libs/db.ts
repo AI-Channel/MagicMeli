4a28e7ffe4bc5a3538a44013a128cbec8a828c64
Mio Akiyama <AkiyamaMio@live.cn>
2024-07-01 10:18:39 +0800

    remove test file

--- magic-meli-service-test/src/libs/db.ts
+++
@@ -1,5 +0,0 @@
-import Database from 'bun:sqlite'
-
-const db = new Database('./src/MagicMeli.db')
-
-function insertTags(tagsArray: string[]) {}
--- magic-meli-service-test/src/models/image.ts
+++
@@ -1,8 +0,0 @@
-export interface ImageMeta {
-  id: string
-  title: string
-  uploadDate: string
-}
-export interface Image extends ImageMeta {
-  imageBase64: string
-}
